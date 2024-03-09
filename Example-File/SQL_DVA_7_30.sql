3749fb1cbb56d0b7537f8c21a9ac3f833e413cc5
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-03-09 07:55:09 +0530

    Update SQL_DVA_7_30.sql

--- Example-File/SQL_DVA_7_30.sql
+++ Example-File/SQL_DVA_7_30.sql
@@ -56,6 +56,58 @@ insert into info values(3,'joy2','mumbai',25000);
 insert into info values(4,'joy3','mumbai',25000);
 
 
+-- Date : 9 March 2023 
+-- insert query 
+
+use p4n_employees;
+
+create table client (
+	client_id int primary key,
+    name varchar(10),
+    city varchar(10)
+);
+
+insert into client values(101,'joy','mumbai');
+
+select * from client;
+
+create table client1 (
+	client_id int primary key auto_increment,
+    name varchar(10),
+    city varchar(10)
+);
+
+insert into client1 (name,city) values('joy','mumbai');
+
+select * from client1;
+
+-- where 
+
+use sakila;
+select * from payment;
+
+select * from payment
+where amount = 4.99;
+
+-- and operator
+select * from payment
+where amount = 4.99 and staff_id = 1;
+
+-- or operator
+
+select * from payment
+where amount = 4.99 or staff_id = 1;
+
+-- update query 
+
+update payment
+set amount = 12.0
+where payment_id = 1;
+
+select * from payment 
+where payment_id = 1;
+
+
 
 
 
