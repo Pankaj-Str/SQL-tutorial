2eb9e8de140579260115222920d3b302edd89d8f
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-03-19 08:20:56 +0530

    Update SQL_DVA_7_30.sql

--- Example-File/SQL_DVA_7_30.sql
+++ Example-File/SQL_DVA_7_30.sql
@@ -341,3 +341,121 @@ SELECT e.FirstName AS EmployeeFirstName, e.LastName AS EmployeeLastName,
        m.FirstName AS ManagerFirstName, m.LastName AS ManagerLastName
 FROM Employees e
 LEFT JOIN Employees m ON e.ManagerID = m.EmployeeID;
+
+
+-- 19 March 2023
+
+-- truncate table 
+-- having 
+-- useful function
+
+use cwp;
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100)
+);
+
+INSERT INTO employees(employee_id, first_name, last_name, email)
+VALUES
+    (1, 'Rohit', 'Gupta', 'rohit@codeswithpankaj.com'),
+    (2, 'Pankaj', 'Sharma', 'pankaj@codeswithpankaj.com'),
+    (3, 'Nishant', 'Verma', 'nishant@codeswithpankaj.com'),
+    (4, 'Neelam', 'Singh', 'neelam@codeswithpankaj.com'),
+    (5, 'Tanvi', 'Patel', 'tanvi@codeswithpankaj.com'),
+    (6, 'Kiran', 'Yadav', 'kiran@codeswithpankaj.com'),
+    (7, 'Ruby', 'Jain', 'ruby@codeswithpankaj.com'),
+    (8, 'Priti', 'Mehta', 'priti@codeswithpankaj.com'),
+    (9, 'Aditi', 'Raj', 'aditi@codeswithpankaj.com'),
+    (10, 'Kritek', 'Agarwal', 'kritek@codeswithpankaj.com');
+    
+
+select * from employees;
+
+-- Concatenates first and last names.
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM employees;
+
+-- Converts email addresses to uppercase and lowercase.
+SELECT UPPER(email) AS uppercase_email, LOWER(email) AS lowercase_email FROM employees;
+
+-- Extracts the first three characters of the email
+
+SELECT SUBSTRING(email, 1, 5) AS email_prefix FROM employees;
+
+-- LENGTH(str) or CHAR_LENGTH(str):
+SELECT first_name, LENGTH(first_name) AS name_length FROM employees;
+
+-- TRIM([{BOTH | LEADING | TRAILING} [remstr] FROM] str)
+
+SELECT TRIM(BOTH ' ' FROM email) AS trimmed_email FROM employees;
+
+-- REPLACE(str, old_substring, new_substring):
+
+SELECT REPLACE(email, 'codeswithpankaj.com', 'p4n.in') AS modified_email FROM employees;
+
+
+use sakila;
+
+
+
+-- ROUND(number, decimals)
+SELECT amount, ROUND(amount) AS rounded_id FROM payment;
+
+-- CEIL(number) and FLOOR(number):
+
+SELECT CEIL(amount) AS ceil_id, FLOOR(amount) AS floor_id FROM payment;
+
+-- ABS(number)
+SELECT ABS(amount) AS absolute_id FROM payment;
+
+-- RAND():
+
+SELECT payment_id,  ROUND(RAND()*78) AS random_number FROM payment;
+
+-- SQRT(number):
+SELECT payment_id,  sqrt(amount)  FROM payment;
+
+
+-- POWER(number, exponent):
+
+SELECT amount,  power(amount,2)  FROM payment;
+
+-- MOD(number, divisor):
+
+SELECT payment_id,  mod(amount,2)  FROM payment;
+
+
+select * from payment;
+-- CURDATE():
+SELECT CURDATE();
+-- DATEDIFF(date1, date2):
+
+SELECT payment_id, DATEDIFF( last_update,payment_date) AS days_until FROM payment;
+
+SELECT payment_id, DATEDIFF( CURDATE(),payment_date) AS days_until FROM payment;
+
+
+-- DATE_ADD(date, INTERVAL value unit):
+
+-- Adds a specific interval to a date.
+SELECT payment_id, payment_date, DATE_ADD(payment_date, INTERVAL 7 DAY) AS new_date FROM payment;
+
+-- DATE_SUB(date, INTERVAL value unit):
+
+-- Subtracts a specific interval from a date.
+SELECT payment_id, payment_date, DATE_SUB(payment_date, INTERVAL 10 MONTH) AS new_date FROM payment;
+
+SELECT payment_id, payment_date, DATE_SUB(payment_date, INTERVAL 10 year) AS new_date FROM payment;
+
+SELECT payment_id, payment_date, DATE_ADD(payment_date, INTERVAL 10 year) AS new_date FROM payment;
+
+-- DATE_FORMAT(date, format):
+
+-- Formats a date as a string according to a specified format.
+SELECT payment_id, DATE_FORMAT(payment_date, '%Y-%m-%d') AS formatted_date FROM payment;
+
+
+SELECT NOW() AS current_datetime;
+SELECT CURTIME()
+
