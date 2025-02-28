4c1850fcbc30220d33cded3fd2c21682202ddce1
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-02-28 15:03:36 +0530

    Update SQL_2_00.sql

--- Example-File/SQL_2_00.sql
+++ Example-File/SQL_2_00.sql
@@ -133,17 +133,74 @@ select customername,city,state,country,creditlimit from customers;
 select customername,city,state,country,creditlimit , creditlimit - 1500 as Update_amount 
 from customers where country = "USA" and city = "NYC";
 
+-- Date : 28 Feb 2025
 -- 11. SQL LIKE Clause
+use classicmodels;
+select * from customers where customername like "a%";
+select * from customers where customername like "%a%";
+select * from customers where customername like "%s";
 
--- 12. SQL TOP Clause
+-- 12. SQL TOP Clause - Limit 
+
+select * from customers limit 5;
 
 -- 13. SQL ORDER BY Clause
 
+select * from customers order by customername asc;
+select * from customers order by customername desc;
+select * from customers order by creditlimit desc;
+select * from customers order by creditlimit asc;
+
 -- 14. SQL Group By
+
+select country from customers group by country;
+
+-- count()
+select country , count(country) as total_number from customers group by country;
+select country , count(country)  as total_number from customers group by country order by count(country) desc;
+
+-- sum()
+select sum(creditlimit) as total from customers;
+
+
+select country , count(country) as total_number , sum(creditlimit) from customers group by country;
 
 -- 15. SQL Distinct Keyword
 
--- 16. SQL Constraints
+select distinct country from customers;
+
+-- 16. SQL Constraints key
+
+
+use cwpc;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50)
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+-- Inserting records into the "Customers" and "Orders" tables
+INSERT INTO Customers (CustomerID, FirstName, LastName)
+VALUES
+    (1, 'Pankaj', 'Sharma'),
+    (2, 'Nishant', 'Patel');
+    
+
+INSERT INTO Orders (OrderID, CustomerID, OrderDate)
+VALUES
+    (101, 1, '2023-01-15'),
+    (102, 2, '2023-02-20'); 
+    
+select * from customers;    
+select * from orders;    
 
 -- 17. SQL Joins
 
