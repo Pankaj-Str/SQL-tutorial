e7ae83c94394ea89cb3b9aab78fb69ebe9cac88c
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-03-04 14:38:55 +0530

    Update SQL_2_00.sql

--- Example-File/SQL_2_00.sql
+++ Example-File/SQL_2_00.sql
@@ -204,6 +204,67 @@ select * from orders;
 
 -- 17. SQL Joins
 
+-- 17. SQL Joins
+
+-- Date : 04 March 2025
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100)
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    TotalAmount DECIMAL(10, 2)
+);
+
+INSERT INTO Customers (CustomerID, FirstName, LastName, Email)
+VALUES
+    (1, 'Pankaj', 'Sharma', 'pankaj@codeswithpankaj.com'),
+    (2, 'Nishant', 'Patel', 'nishant@codeswithpankaj.com'),
+    (3, 'Kiran', 'Desai', 'kiran@codeswithpankaj.com'),
+    (4, 'Tanvi', 'Mehta', 'tanvi@codeswithpankaj.com'),
+    (5, 'Kritek', 'Singh', 'kritek@codeswithpankaj.com'),
+    (6, 'Riya', 'Jain', 'riya@codeswithpankaj.com');
+
+INSERT INTO Orders (OrderID, CustomerID, OrderDate, TotalAmount)
+VALUES
+    (101, 1, '2023-01-15', 250.00),
+    (102, 2, '2023-02-20', 120.50),
+    (103, 3, '2023-03-10', 320.75),
+    (105, 4, '2023-05-12', 210.00),
+    (107, 7, '2023-10-11', 510.00),
+    (108, 8, '2023-08-15', 910.00);
+    
+select * from customers;    
+select * from orders; 
+-- left join
+select customers.firstname,customers.email,orders.totalamount, orders.orderdate
+from customers left join orders
+on customers.customerid = orders.customerid;
+   
+-- right join
+select customers.firstname,customers.email,orders.totalamount, orders.orderdate
+from customers right join orders
+on customers.customerid = orders.customerid;
+
+-- inner join
+select customers.firstname,customers.email,orders.totalamount, orders.orderdate
+from customers inner join orders
+on customers.customerid = orders.customerid;
+
+-- full join
+select customers.firstname,customers.email,orders.totalamount, orders.orderdate
+from customers left join orders
+on customers.customerid = orders.customerid
+union
+select customers.firstname,customers.email,orders.totalamount, orders.orderdate
+from customers right join orders
+on customers.customerid = orders.customerid;
+
 -- 18. SQL Unions Clause
 
 -- 19. SQL TRUNCATE TABLE
