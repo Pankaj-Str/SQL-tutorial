80d6bb42a79397c954d88b82434f42e1892468e3
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-04-18 07:39:29 +0530

    Add files via upload

---
+++ Example-File/all_in_one.sql
@@ -0,0 +1,56 @@
+-- introduction of Database 
+-- intro - Sql
+-- Create Database 
+-- drop Database
+-- select Database
+-- create table 
+-- insert query 
+-- where 
+-- and operator
+-- or operator
+-- update query 
+-- delete 
+-- like 
+-- top
+-- order by
+-- group by
+-- distinct keyword
+-- Constraints
+-- join
+-- unions claus
+-- truncate table 
+-- having 
+-- view 
+-- useful function
+
+
+-- Date - 18 April 2025
+-- show list of database 
+show databases;
+-- Create Database 
+create database cwpc01;
+-- drop Database
+drop database cwpc01;
+-- select Database
+use cwpc01;
+-- create table 
+-- column name  
+-- data 
+-- table name 
+create table 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
