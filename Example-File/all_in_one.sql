a016069d7a6a40831235d36637e01e3e1f59c299
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-04-23 07:40:21 +0530

    Update all_in_one.sql

--- Example-File/all_in_one.sql
+++ Example-File/all_in_one.sql
@@ -48,14 +48,72 @@ create table Employee(
 show tables;
 
 -- show table 
-select * from Employee
+select * from Employee;
  
 
 
+-- Date 23 - April - 2025
+-- show list of database
+show databases;
+-- create database
+create database Company;
+-- drop database (delete database)
+-- drop database Company;
+-- select database 
+use Company;
+-- create table 
+create table employee(
+	id int,
+    name varchar(50),
+    dob date,
+    salary int,
+    dept enum('sales','hr','it','other'),
+    city varchar(50),
+    email varchar(50)
+);
+
+-- show list of table
+show tables;
+
+-- open table and show record
+select * from employee;
+
+-- insert record into table 
+insert into employee (id,name,dob,salary,dept,city,email) 
+value(1,'Joy','1990-05-23',34000,'sales','mumbai','joy@cwpc.in');
+
+-- insert multipal records
+insert into employee (id,name,dob,salary,dept,city,email) 
+values (2,'Toy','1990-05-13',56000,'hr','pune','toy@cwpc.in'),
+(3,'Rohan','1985-05-13',55000,'hr','pune','rohan@cwpc.in'),
+(4,'Tina','1958-05-13',51000,'sales','surat','tina@cwpc.in'),
+(5,'Nir','1963-05-13',52000,'other','bhuj','nir@cwpc.in'),
+(6,'Yuo','2000-05-13',50000,'hr','goa','yuo@cwpc.in');
 
 
 
 
+-- use primary key
+
+create table employee1(
+	id int primary key,
+    name varchar(50),
+    dob date,
+    salary int,
+    dept enum('sales','hr','it','other'),
+    city varchar(50),
+    email varchar(50)
+);
+
+-- show list of table
+show tables;
+
+-- open table and show record
+select * from employee1;
+
+-- insert record into table 
+insert into employee1 (id,name,dob,salary,dept,city,email) 
+value(2,'Joy','1990-05-23',34000,'sales','mumbai','joy@cwpc.in');
 
 
 
