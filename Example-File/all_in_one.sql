1048554f40a8b937dc33fcf5f5d0991243a8e142
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-04-30 07:50:03 +0530

    Update all_in_one.sql

--- Example-File/all_in_one.sql
+++ Example-File/all_in_one.sql
@@ -193,5 +193,103 @@ select distinct count(country) from customers;
 
 select sum(creditlimit) as total_cd from customers  where country = "USA";
 
+-- Date : 30 April 2025
+-- Constraints
+create database cwpc009;
+
+use cwpc009;
+
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100) UNIQUE
+);
+
+-- Inserting records into the "Students" table
+INSERT INTO Students (FirstName, LastName, Email)
+VALUES
+    ('Pankaj', 'Sharma', 'pankaj@codeswithpankaj.com'),
+    ('Nishant', 'Patel', 'nishant@codeswithpankaj.com'),
+    ('Kiran', 'Desai', 'kiran@codeswithpankaj.com');
+    
+select * from students;    
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    mobile VARCHAR(50) unique
+);
+
+-- Inserting records into the "Customers" and "Orders" tables
+INSERT INTO Customers (CustomerID, FirstName, mobile)
+VALUES
+    (1, 'Pankaj', '78789714'),
+    (2, 'Nishant', '44789465');
+    
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+); 
+
+INSERT INTO Orders (OrderID, CustomerID, OrderDate)
+VALUES
+    (101, 1, '2023-01-15'),
+    (102, 2, '2023-02-20');   
 
 
+INSERT INTO Customers (CustomerID, FirstName, mobile)
+VALUES
+    (3, 'Joy', '79789714');
+
+INSERT INTO Orders (OrderID, CustomerID, OrderDate)
+VALUES
+    (105, 3, '2023-01-15');
+
+select * from orders;
+select * from customers;
+
+-- unions claus
+
+
+CREATE TABLE Customers1 (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100)
+);
+
+CREATE TABLE Suppliers (
+    SupplierID INT PRIMARY KEY,
+    CompanyName VARCHAR(100),
+    Email VARCHAR(100)
+);
+
+INSERT INTO Customers1 (CustomerID, FirstName, LastName, Email)
+VALUES
+    (1, 'Pankaj', 'Sharma', 'pankaj@codeswithpankaj.com'),
+    (2, 'Nishant', 'Patel', 'nishant@codeswithpankaj.com'),
+    (3, 'Kiran', 'Desai', 'kiran@codeswithpankaj.com'),
+    (4, 'Tanvi', 'Mehta', 'tanvi@codeswithpankaj.com'),
+    (5, 'Kritek', 'Singh', 'kritek@codeswithpankaj.com');
+
+INSERT INTO Suppliers (SupplierID, CompanyName, Email)
+VALUES
+    (101, 'ABC Inc.', 'abc@codeswithpankaj.com'),
+    (102, 'XYZ Ltd.', 'xyz@codeswithpankaj.com'),
+    (103, 'Company A', 'companya@p4n.in');
+    
+    
+select * from customers1;    
+select * from suppliers;    
+
+
+SELECT Email FROM Customers1
+UNION
+SELECT Email FROM Suppliers;
+
+-- truncate table 
+
+truncate table customers;
